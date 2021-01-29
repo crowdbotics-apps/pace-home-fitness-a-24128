88de7a6318f9e058dd3a2ea4fafc3c60900e68e1
Crowdbotics <team@crowdbotics.com>
2021-01-29 00:56:58 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/pace_home_fitness_a_24128/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pace_home_fitness_a_24128;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "pace_home_fitness_a_24128";
+    }
+}
